76b8af65509b1944e71c0789766aa02ff4bd5854
villa7 <villa7.kluge5@gmail.com>
2014-03-07 00:13:43 -0500

    moar pages

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 10) {
-            $("#wrapper").addClass("scrolled-1");
-        } else {
-            $("#wrapper").removeClass("scrolled-1");
-        }
-    });
+    $('.wrapper').colorScroll({
+    colors: [{
+        color: '#151b54',
+        position: '0%'
+    }, {
+        color: '#4b0082',
+        position: '40%'
+    }, {
+        color: '#254117',
+        position: '80%'
+    }]
 });
+});
+
+scrollTo = function(section) {
+    if (section === 2) {
+        $(".main").moveTo(2)
+    } else {
+        $(".main").moveTo(3)
+    }
+}
