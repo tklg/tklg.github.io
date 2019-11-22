fb170e0c6c320abc9123c98f2b787900a85ff8b2
unknown <villa7.kluge5@gmail.com>
2019-11-22 11:58:12 -0500

    feat: linkedin and resume links

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -15,14 +15,20 @@ export default class Header extends React.Component {
           </section>
           <section className='sm-info flex-container flex-center'>
             <div className='v-center'>
-              <a href='https://github.com/villa7' target='_blank'>
+              <a href='https://github.com/villa7' target='_blank' title='github'>
                 <Icon icon='github' />
               </a>
-              <a href='https://codepen.io/villa7' target='_blank'>
+              <a href='https://codepen.io/villa7' target='_blank' title='codepen'>
                 <Icon icon='codepen' />
               </a>
-              <a href='https://stackoverflow.com/users/3605190/villa7?tab=profile' target='_blank'>
+              <a href='https://stackoverflow.com/users/3605190/villa7?tab=profile' target='_blank' title='stack overflow'>
                 <Icon icon='stackoverflow' />
+              </a>
+              <a href='https://www.linkedin.com/in/theodore-kluge/' target='_blank' title='linkedin'>
+                <Icon icon='linkedin' />
+              </a>
+              <a href='https://mega.nz/#!jgpDxKSJ!hmCuuelRjRmHvv2TzVXdfk_J1XBm81O0NGZN8a0zJYA' target='_blank' title='resume'>
+                <Icon icon='resume' />
               </a>
             </div>
           </section>
